0253c9d0e0b8d3073f249d17cafb0fd7390abcfa
Taylor Seamans <taylor.seamans@yahoo.com>
2022-11-10 09:17:18 -0800

    Fix manifest'

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,15 +47,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `taylor-seamans-portfolio`,
+        short_name: `taylor-seamans-portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#FFFBF5`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
   ],
